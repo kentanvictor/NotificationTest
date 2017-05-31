8f1603ed61f96252c7d4d5b9de9363122005cbaf
kentanvictor <2030457926@qq.com>
2017-05-31 20:42:30 +0800

    PendingIntent

--- app/src/main/java/com/example/dell/notificationtest/MainActivity.java
+++ app/src/main/java/com/example/dell/notificationtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dell.notificationtest;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -23,12 +25,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         switch (v.getId()){
             case R.id.send_notice:
+                Intent intent = new Intent(this,NotificationActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
                 NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 Notification notification = new NotificationCompat.Builder(this)
                         .setContentTitle("This is content title")
                         .setContentText("This is content text")
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentIntent(pi)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                         .build();
                 manager.notify(1,notification);
